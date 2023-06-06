1e30f760e8fb58a43d8ef95f8ea7a2e36423d986
ISMAEL DE MELLO TELLES <82053092+ismaelmello@users.noreply.github.com>
2023-06-06 14:55:16 +0000

    o

--- http-sever.js
+++ http-sever.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 const app = express()
 
 
-app.use(express.static(_dirbame + '/public'))
+app.use(express.static(__dirname + '/public'))
 app.use("/teste", (req, res) => {
-    fs.writeFileSync(
-        
-    )
+    const { file, texto } = req.query
+    fs.writeFileSync( file, texto)
+    req.setEncoding(new Data())
 })
 app.listen(3000, () => console.log("Serve rodando"))
