88f1d340ec22e9f2f89f560eb3ab1c9ae1274857
ISMAEL DE MELLO TELLES <82053092+ismaelmello@users.noreply.github.com>
2023-06-27 18:15:28 +0000

    ...

--- public/main.js
+++ public/main.js
@@ -1,25 +1,31 @@
 const domMovieList = document.querySelector("ul.movie-list")
-    
+const domAlterar = document.querySelector("form.alterar")
+
+// ---✀------------------------------------------------------------------
+
 async function listarTodosOsFilmes() {
   const response = await fetch("/movies")
   const movies = await response.json()
+  domMovieList.innerHTML = ""
   movies.forEach(movie => {
     domMovieList.innerHTML += `  
       <li>
-        <strong>title</strong>
-        ${movie.title}
+        <div>
+          <strong>Title:</strong> ${movie.title}
+        </div>
         <ul>
           <li>
-            <strong>source</strong>
-            ${movie.source}
+            <strong>source:</strong> ${movie.source}
           </li>
           <li>
-            <strong>thumb</strong>
-            ${movie.thumb}
+            <strong>thumb:</strong> ${movie.thumb}
           </li>
           <li>
-            <strong>description</strong>
-            ${movie.description}
+            <strong>description:</strong> ${movie.description}
+          </li>
+          <li>
+            <button>excluir</button>
+            <button>alterar</button>
           </li>
         </ul>
       </li>
@@ -28,3 +34,35 @@ async function listarTodosOsFilmes() {
 }
 
 listarTodosOsFilmes()
+
+// ---✀------------------------------------------------------------------
+
+domAlterar.addEventListener("submit", async ev => {
+  ev.preventDefault()
+  ev.stopPropagation()
+  ev.stopImmediatePropagation()
+  ev.stopenter()
+
+  const body = JSON.stringify({
+    title: domAlterar.title.value,
+    source: domAlterar.source.value,
+    description: domAlterar.description.value,
+    thumb: domAlterar.thumb.value,
+  })
+
+  const response = await fetch("/movies", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body
+  })
+  
+  const result = await response.json()
+  domAlterar.reset()
+
+   listarTodosOsFilmes()
+})
+
+// ---✀------------------------------------------------------------------
+
+
+
